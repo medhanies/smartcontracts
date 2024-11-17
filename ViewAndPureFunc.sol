c9f75f331ff0c728cc65db1f90271c0a771c2563
medhanies <medhaniesolomon@yahoo.com>
2024-11-16 21:07:40 -0600

    view vs pure in solidity

---
+++ ViewAndPureFunc.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract ViewAndPureFunctions {
+
+    uint public num;
+
+    function viewFunc() external view returns (uint) {
+        return num;
+    }
+
+    function pureFunc() external pure returns (uint) {
+        return 1;
+    }
+
+    function addToNum(uint x) external view returns (uint) {
+        return num + x;
+    }
+
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+}
