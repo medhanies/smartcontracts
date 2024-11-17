a93b09e80e481293afac310c14ff17dfc62c8356
medhanies <medhaniesolomon@yahoo.com>
2024-11-16 21:16:04 -0600

    default values in solidity

---
+++ DefaultValues.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract DefaultValues {
+
+    bool public b; // false
+    uint public u; // 0
+    int public i; // 0
+    address public a; // 0x0000000
+    bytes32 public b32; // 0x0000000000000000000
+    
+
+
+}
