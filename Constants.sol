66af84d9e043da49e4bbddd022339e813b66f80b
medhanies <medhaniesolomon@yahoo.com>
2024-11-16 21:26:55 -0600

    constant variables save gas

---
+++ Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // 373 gas
+    uint public constant MY_UINT = 500;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // 2483 gas
+}
