f32a257d1776345c40aa7984b25e0ed94314ead6
medhanies <medhaniesolomon@yahoo.com>
2024-11-16 20:44:48 -0600

    functions in solidity

---
+++ Func.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+contract FunctionIntro {
+
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+
+    function sub(uint x, uint y) external pure returns (uint) {
+        return x - y;
+    }
+
+}
