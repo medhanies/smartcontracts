0fb60b6aab9403fc03b2f2197e9a401cfae65a8f
medhanies <medhaniesolomon@yahoo.com>
2024-11-17 22:06:59 -0600

    if else in solidity

---
+++ IfElse.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract IfElse {
+
+
+    function example(uint _x) external pure returns (uint) {
+        if(_x < 10) {
+            return 1;
+        } else if( _x < 20){
+            return 2;
+        }else {
+            return 3;
+            }
+    }
+
+    function ternary(uint _x) external pure returns (uint) {
+
+
+        return _x < 10 ? 1:2;
+    }
+}
