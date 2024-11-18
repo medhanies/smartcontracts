825a78f0ab7e4b2c6df903423ca0f4bec2ad0916
medhanies <medhaniesolomon@yahoo.com>
2024-11-17 23:27:46 -0600

    ownable contract allows owner to relinquish ownership to new address

---
+++ Ownable.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+// state variables
+// global variables
+// function modifier
+// function
+// error handling
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "invalid address");
+        owner = _newOwner;
+    }
+
+    function onlyOwnerCanCallThisFunc() external onlyOwner {
+        // code
+    }
+
+    function anyOneCanCall() external {
+        // code
+    }
+}
